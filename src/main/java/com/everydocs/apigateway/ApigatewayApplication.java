b1e9f8c196a17f634184b3c1d18898e5013c14bd
snitavetsiv <snitavets.ilya@gmail.com>
2020-02-29 14:36:17 +0300

    init apigatewa

---
+++ src/main/java/com/everydocs/apigateway/ApigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.everydocs.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ApigatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApigatewayApplication.class, args);
+  }
+}
