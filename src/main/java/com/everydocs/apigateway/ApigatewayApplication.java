3421fc0bb8d1e486355ffe18abbaed79923c3b3a
snitavetsiv <snitavets.ilya@gmail.com>
2020-03-04 22:42:29 +0300

    add consul

--- src/main/java/com/everydocs/apigateway/ApigatewayApplication.java
+++ src/main/java/com/everydocs/apigateway/ApigatewayApplication.java
@@ -3,8 +3,12 @@ package com.everydocs.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class ApigatewayApplication {
 
   public static void main(String[] args) {
