b5ba2b8322943c1182ef2747811ee1d4d588a330
snitavetsiv <snitavets.ilya@gmail.com>
2020-03-10 22:19:22 +0300

    add jacoco

---
+++ src/test/java/com/everydocs/apigateway/ApigatewayApplicationTest.java
@@ -0,0 +1,13 @@
+package com.everydocs.apigateway;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApigatewayApplicationTest {
+
+  @Test
+  void main() {
+    // empty test
+  }
+}
